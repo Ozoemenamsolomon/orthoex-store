2d4d94a26b28aea922e97feb77cfa0d2e93500bd
Solozo <ozoemenamsolomon@yahoo.com>
2022-01-19 18:47:26 +0100

    remove tag id variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,14 +17,14 @@ function MyApp({ Component, pageProps }: AppProps) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
+            })(window,document,'script','dataLayer', 'GTM-WT8K4RZ');
           `
 							: '',
 				}}
 			/>
 			<noscript>
 				<iframe
-					src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+					src={`https://www.googletagmanager.com/ns.html?id=GTM-WT8K4RZ`}
 					height="0"
 					width="0"
 					style={{ display: 'none', visibility: 'hidden' }}
