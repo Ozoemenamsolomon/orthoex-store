4d8c6d13ea2f0ca92194b8743f2b1ac36d13d232
Solozo <ozoemenamsolomon@yahoo.com>
2022-01-19 19:43:48 +0100

    replace with GA ID and config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,27 +9,23 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<>
 			<Script
 				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=G-JJF85CLTXB`}
+			/>
+			<Script
+				strategy="afterInteractive"
 				dangerouslySetInnerHTML={{
 					__html:
 						process.env.NODE_ENV === 'production'
 							? `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', 'GTM-WT8K4RZ');
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+						  
+							gtag('config', 'G-JJF85CLTXB');
           `
 							: '',
 				}}
 			/>
-			<noscript>
-				<iframe
-					src={`https://www.googletagmanager.com/ns.html?id=GTM-WT8K4RZ`}
-					height="0"
-					width="0"
-					style={{ display: 'none', visibility: 'hidden' }}
-				/>
-			</noscript>
 			<Component {...pageProps} />
 		</>
 	);
