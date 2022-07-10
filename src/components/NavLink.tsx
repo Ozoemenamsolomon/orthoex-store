f7b10ab9b5163acf17da3097dce3bf86621d9a37
Solozo <ozoemenamsolomon@yahoo.com>
2022-07-10 22:08:10 +0200

    import images and icons

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,13 +1,26 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
-import Link from 'next/link';
 
-const NavLink: FC<{ to: string; name: string }> = ({ name, to }) => {
+const NavLink: FC<{ to: string; name: string; icon?: any }> = ({
+	name,
+	to,
+	icon,
+}) => {
 	const router = useRouter();
 
 	return to.startsWith('/') ? (
 		<Link href={to}>
-			<a className={router.pathname == to ? 'active' : ''}>{name}</a>
+			<a className={router.pathname == to ? 'active' : ''}>
+				{icon ? (
+					<div style={{ position: 'relative', width: 24, aspectRatio: '1' }}>
+						<Image layout="fill" src={icon} />
+					</div>
+				) : (
+					name
+				)}
+			</a>
 		</Link>
 	) : (
 		<a href={to}>{name}</a>
