5fd203413034fe6a0c623909f2d5f184524a8b99
Solozo <ozoemenamsolomon@yahoo.com>
2022-08-06 16:52:22 +0200

    create rename js

---
+++ src/assets/new/images/rename.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const dir = path.dirname(__filename);
+
+// log list of files in directory
+fs.readdir(dir, (err, files) => {
+	if (err) throw err;
+
+	files.forEach((file) => {
+		const oldFileName = path.join(dir, file);
+		fs.rename(oldFileName, oldFileName.replace('product', 'category'), (err) =>
+			console.log({ err })
+		);
+	});
+});
