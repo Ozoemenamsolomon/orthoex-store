67eb5e25ca890f3c3c67cdb82dc0f0cc4c232d04
Solozo <ozoemenamsolomon@yahoo.com>
2022-10-06 03:14:06 +0200

    install tab package

--- src/pages/composites/products/polyester-resin.tsx
+++ src/pages/composites/products/polyester-resin.tsx
@@ -22,6 +22,8 @@ import { CTA, SocialCTA } from '@components/Header';
 import { Facebook, Instagram, Twitter } from '@styled-icons/bootstrap';
 import DataSheet from '@assets/new/icons/DataSheet';
 import { helps } from '@components/sections/NeedHelpSection';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const orderBenefits: ServiceCardType[] = [
 	{
@@ -261,7 +263,21 @@ const SingleProduct: NextPage<{ product: ProductDataType }> = ({ product }) => {
 						))}
 					</div>
 				</SooSection>
-				<SooSection BGColor="white">3</SooSection>
+				<SooSection BGColor="white">
+					<Tabs>
+						<TabList>
+							<Tab>Title 1</Tab>
+							<Tab>Title 2</Tab>
+						</TabList>
+
+						<TabPanel>
+							<h2>Any content 1</h2>
+						</TabPanel>
+						<TabPanel>
+							<h2>Any content 2</h2>
+						</TabPanel>
+					</Tabs>
+				</SooSection>
 				<SooSection BGColor="white">
 					<Title>Do you need help?</Title>
 					<div style={{ display: 'grid', gap: '2rem' }}>
