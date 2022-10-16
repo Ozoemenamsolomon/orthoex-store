585e371ad4304edda8115d50f5162753714b70d5
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-10-16 23:49:29 +0200

    created a button shared component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,48 @@
-import type { NextPage } from 'next';
-import React from 'react';
-import HeaderBG from '@assets/images/header-background.png';
-import { CTA } from '@components/Header';
-import { HeroComp as Hero } from '@components/Hero';
-import HeroContent from '@components/HeroContent';
-import {
-	Container, 
-} from '@components/styled';
-import ServiceStandard from '@components/ServiceStandard';
-import InfoSection from '@components/InfoSection';
-import InfoText from '@components/InfoText';
-import InfoTestmonial from '@components/InfoTestimonial';
-import ProductVertical from '@components/ProductVertical';
-
-
+import type { NextPage } from "next";
+import React from "react";
+import HeaderBG from "@assets/images/header-background.png";
+import { CTA } from "@components/Header";
+import { HeroComp as Hero } from "@components/Hero";
+import HeroContent from "@components/HeroContent";
+import { Container } from "@components/styled";
+import ServiceStandard from "@components/ServiceStandard";
+import InfoSection from "@components/InfoSection";
+import InfoText from "@components/InfoText";
+import InfoTestmonial from "@components/InfoTestimonial";
+import ProductVertical from "@components/ProductVertical";
+import CallButton from "@components/CallButton";
 
 const Home: NextPage = () => {
-	const findRep = () => {};
-	return (
-		<>
-			<Hero bg={HeaderBG}>
-				<HeroContent
-					title={'Your preferred<br />partner of choice'}
-					claim={
-						'We are facilitating Prosthetics, Orthopaedic devices and composite materials distribution in West Africa. <br /> <br /> Speak with an account manager in your region'
-					}
-					cta={<CTA onClick={findRep}>Find a representative</CTA>}
-				/>
-			</Hero>
-			<Container bg="white" paddingMultiplier={0}>
-				<ServiceStandard />
-				<InfoSection />
-				<ProductVertical />
-				<InfoText
-					title={'At OrthoEx, we enable businesses like yours to succeed'}
-					description={
-						'We are facilitating Prosthetics, Orthopaedic devices and composite materials distribution in West Africa.'
-					}
-				/>
-				<InfoTestmonial />
-			</Container>
-		</>
-	);
+  const findRep = () => {};
+  return (
+    <>
+    <Container bg="white" paddingMultiplier={0}>
+      <Hero bg={HeaderBG}>
+        <HeroContent
+          title={"Your preferred<br />partner of choice"}
+          claim={
+            "We are facilitating Prosthetics, Orthopaedic devices and composite materials distribution in West Africa. <br /> <br /> Speak with an account manager in your region"
+          }
+          cta={<CTA onClick={findRep}>Find a representative</CTA>}
+        />
+      </Hero>
+
+      <ServiceStandard />
+      <InfoSection />
+      <ProductVertical />
+      <InfoText
+        title={"At OrthoEx, we enable businesses like yours to succeed"}
+        description={
+          "We collaborate with our clients every day, delivering solutions and technologies to help them adapt to changing market conditions and stay ahead of the competition."
+        }
+      />
+      <InfoTestmonial />
+
+      <CallButton />
+    </Container>
+    </>
+
+  );
 };
 
 export default Home;
