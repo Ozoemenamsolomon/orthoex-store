5c649f1734cf17fef852c2d7a40fd3e7f7382d5b
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-10-17 23:40:54 +0200

    created the call button for mobile view in home

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,22 +3,22 @@ import styled from "styled-components"
 import React from "react"
 
 interface ButtonProps {
-  width: string
+  width?: string
   primary?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({width, children, primary}) => {
   return (
-    <StyledCallButton width={width} primary={primary}>
+    <StyledButton width={width} primary={primary}>
       {children}
-    </StyledCallButton>
+    </StyledButton>
   )
 }
 
 export default Button
 
 
-const StyledCallButton = styled.button<ButtonProps>`
+export const StyledButton = styled.button<ButtonProps>`
   color: ${({primary}) => primary ? "white" : "var(--oex-orange)" };
   background-color: ${({primary}) => primary ? "var(--oex-orange)" : "white"  };
   font: inherit;
@@ -26,6 +26,7 @@ const StyledCallButton = styled.button<ButtonProps>`
   width: ${({width}) => width ? width : "" };
   border-radius: 0.2rem;
   border: 0.09rem solid var(--oex-orange);
+  cursor: pointer;
 
   &:hover {
     color: ${({primary}) => primary ? "var(--oex-orange)" : "white" } ;
