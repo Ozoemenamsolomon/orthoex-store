90acb1f4a4d02b2e6fd8a63b2cbbf692b078496c
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-10-24 00:42:05 +0200

    company overview mobile view created

--- src/components/About/AboutDetail.tsx
+++ src/components/About/AboutDetail.tsx
@@ -6,11 +6,18 @@ interface AboutDetailProp {
   currentSelected: string;
 }
 
+const descriptionData = [
+  "OrthoEx is a leading prosthetic, orthotic components, and composite materials supplier. Since 2014, we have served thousands of professionals in both the private and public sectors. Our products are designed for our customers' needs across multiple markets in the healthcare and manufacturing industries. In addition, we work closely with prosthetists who provide free artificial limbs to amputees in Nigeria.",
+  "As the market leader, we collaborate with top brands across the globe and offer technical training and consultancy to professionals and organizations on our range of products and technologies.",
+  "We pride ourselves on satisfying our customers and helping them reach their business goals. OrthoEx is on a mission to empower human potential!",
+  "There are many reasons our partners love to work with us. Join us and take advantage of the growing benefits as we continue to grow our competence, quality, and customer experience."
+]
+
 const AboutDetail: React.FC<AboutDetailProp> = ({ currentSelected }) => {
   return (
     <StyledAboutDetail>
       {currentSelected === "overview" ? (
-        <h1>overview</h1>
+        <AboutCard heading={"Company Overview"} descriptionList={descriptionData}/>
       ) : currentSelected === "values" ? (
         <h1>values</h1>
       ) : currentSelected === "business" ? (
