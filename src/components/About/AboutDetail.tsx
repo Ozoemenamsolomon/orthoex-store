33091d1dc39fdcef99361ca80e24ba7ee1c78cb6
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-10-24 00:10:24 +0200

    conditional rendering for the about page created with About Card component

--- src/components/About/AboutDetail.tsx
+++ src/components/About/AboutDetail.tsx
@@ -1,15 +1,29 @@
-import React, { useState, SetStateAction } from "react";
+import React from "react";
+import styled from "styled-components";
+import AboutCard from "./AboutCard";
 
 interface AboutDetailProp {
-  currentSelected: string
+  currentSelected: string;
 }
 
-const AboutDetail: React.FC<AboutDetailProp> = ({currentSelected}) => {
+const AboutDetail: React.FC<AboutDetailProp> = ({ currentSelected }) => {
   return (
-    <div>
-      {currentSelected}
-    </div>
-  )
-}
+    <StyledAboutDetail>
+      {currentSelected === "overview" ? (
+        <h1>overview</h1>
+      ) : currentSelected === "values" ? (
+        <h1>values</h1>
+      ) : currentSelected === "business" ? (
+        <h1>business</h1>
+      ) : null}
+    </StyledAboutDetail>
+  );
+};
 
-export default AboutDetail
+export default AboutDetail;
+
+const StyledAboutDetail = styled.div`
+  margin-top: 2rem;
+  @media (min-width: 768px) {
+  }
+`;
