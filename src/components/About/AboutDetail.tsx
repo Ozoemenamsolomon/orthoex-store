0387e6f2bb6885bf6fff7cb62e884fccf09ccf90
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-10-23 22:16:02 +0200

    props have been passed from the AboutInfo to About Detail

--- src/components/About/AboutDetail.tsx
+++ src/components/About/AboutDetail.tsx
@@ -1,7 +1,13 @@
-function AboutDetail() {
+import React, { useState, SetStateAction } from "react";
+
+interface AboutDetailProp {
+  currentSelected: string
+}
+
+const AboutDetail: React.FC<AboutDetailProp> = ({currentSelected}) => {
   return (
     <div>
-      
+      {currentSelected}
     </div>
   )
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import styled from "styled-components";
 import AboutDetail from "../components/About/AboutDetail";
 import AboutInfo from "../components/About/AboutInfo";
 import { Container } from "@components/styled";
 
 function about() {
+  const [currentSelected, setcurrentSelected] = useState<string>("overview");
+
   return (
     <Container bg="#FAFAFA" paddingMultiplier={0}>
       <StyledAbout>
-        <AboutInfo />
-        <AboutDetail />
+        <AboutInfo setCurrentSelected={setcurrentSelected} />
+        <AboutDetail currentSelected={currentSelected} />
       </StyledAbout>
     </Container>
   );
