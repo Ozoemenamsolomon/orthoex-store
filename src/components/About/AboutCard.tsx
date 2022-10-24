813faf3203d32eca10abe7c3c1b3eb632db9fc80
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-10-24 12:10:17 +0200

    mobile view for the about page completed

--- src/components/About/AboutCard.tsx
+++ src/components/About/AboutCard.tsx
@@ -1,12 +1,18 @@
+import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
+import { DescriptionInfoType } from "./AboutDetail";
+import OurMission from "@assets/new/icons/about/our-mission.svg"
+
+
 
 interface AboutCardType {
   heading: string;
   title?: string;
   image?: any;
   description?: string;
-  descriptionList?: string[]
+  descriptionList?: string[];
+  descriptionInfo?: DescriptionInfoType[];
 }
 
 const AboutCard: React.FC<AboutCardType> = ({
@@ -14,20 +20,25 @@ const AboutCard: React.FC<AboutCardType> = ({
   title,
   image,
   description,
+  descriptionInfo,
   descriptionList,
 }) => {
   return (
     <StyledAboutCard>
       <h1>{heading}</h1>
-      {descriptionList && descriptionList.map((text, index) => (
-        <Text key={index}>{text}</Text>
-      ))}
+      {descriptionList &&
+        descriptionList.map((text, index) => <Text key={index}>{text}</Text>)}
 
-      {description && (
-        <StyledAboutSection>
-          <h4>{title}</h4>
-          <p>{description}</p>
-        </StyledAboutSection>
+      {descriptionInfo && (
+        <StyledAboutWrapper>
+          {descriptionInfo.map((info, index) => (
+            <StyledAboutSection key={`${info.title}-${index}`}>
+              <Image src={info.image} alt={info.title} />
+              <h4>{info.title}</h4>
+              <Text>{info.description}</Text>
+            </StyledAboutSection>
+          ))}
+        </StyledAboutWrapper>
       )}
     </StyledAboutCard>
   );
@@ -36,19 +47,49 @@ const AboutCard: React.FC<AboutCardType> = ({
 export default AboutCard;
 
 const StyledAboutCard = styled.div`
+  margin-bottom: 6rem;
 
-& > h1 {
-  font-size: 1.5rem;
-  border-bottom: 0.1rem solid var(--oex-light-grey);
-  padding-bottom: 1rem;
-  margin-bottom: 1rem;
-  font-weight: 500;
-}
+  & > h1 {
+    font-size: 1.5rem;
+    border-bottom: 0.1rem solid var(--oex-lighter-grey);
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+    font-weight: 500;
+    text-align: center;
+  }
 `;
 const StyledAboutSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  & > h4 {
+    font-size: 1rem;
+    font-weight: 500;
+    margin: 0rem;
+  }
+  & > p {
+    text-align: center;
+    padding: 0rem 2.8rem;
+  }
+
+`;
+
+const StyledAboutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
 
 `;
 
 const Text = styled.p`
   color: var(--oex-dark-grey);
-`
+  font-size: 0.9rem;
+
+  @media(min-width:768px){
+    font-size: 1.1rem;
+  }
+`;
--- src/components/About/AboutDetail.tsx
+++ src/components/About/AboutDetail.tsx
@@ -1,27 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 import AboutCard from "./AboutCard";
+import OurMission from "@assets/new/icons/about/our-mission.svg"
+import OurVision from "@assets/new/icons/about/our-vision.svg"
+import HumanPotential from "@assets/new/icons/about/human-potentials.svg"
+import Innovative from "@assets/new/icons/about/innovative.svg"
+import SwiftResponsive from "@assets/new/icons/about/swift-and-responsive.svg"
+import PositivePassionate from "@assets/new/icons/about/positive-and-pastionate.svg"
+import Excellence from "@assets/new/icons/about/excellence-and-dependable.svg"
+import CustomerFirst from "@assets/new/icons/about/customer-first.svg"
+import Orthopeadeics from "@assets/new/icons/about/orthopaedics.svg"
+import Composites from "@assets/new/icons/about/composites.svg"
 
 interface AboutDetailProp {
   currentSelected: string;
 }
 
-const descriptionData = [
+export interface DescriptionInfoType {
+  image: StaticImageData,
+  title: string,
+  description: string
+}
+
+const overviewDescription = [
   "OrthoEx is a leading prosthetic, orthotic components, and composite materials supplier. Since 2014, we have served thousands of professionals in both the private and public sectors. Our products are designed for our customers' needs across multiple markets in the healthcare and manufacturing industries. In addition, we work closely with prosthetists who provide free artificial limbs to amputees in Nigeria.",
   "As the market leader, we collaborate with top brands across the globe and offer technical training and consultancy to professionals and organizations on our range of products and technologies.",
   "We pride ourselves on satisfying our customers and helping them reach their business goals. OrthoEx is on a mission to empower human potential!",
   "There are many reasons our partners love to work with us. Join us and take advantage of the growing benefits as we continue to grow our competence, quality, and customer experience."
 ]
 
+const missionData: DescriptionInfoType[] = [
+  {image: OurMission,title: "Our Mission", description: "To empower human potential."},
+  {image: OurVision,title: "Our Vision", description: "To be the preferred partner of choice for quality"},
+]
+
+const businessData: DescriptionInfoType[] = [
+  {image: Orthopeadeics,title: "Orthopaedics", description: "We continually enable our clients to achieve efficiency and increased effectiveness of biomedical devices designed for their patients."},
+  {image: Composites, title: "Composites", description: "We collaborate with our clients to help them adapt to changing market conditions and stay ahead of the competition"},
+]
+const valuesData: DescriptionInfoType[] = [
+  {image: CustomerFirst,title: "Customer-first", description: "Our customers are the centre of our corporate decisions. "},
+  {image: HumanPotential,title: "Empower human potentials", description: "We challenge and inspire our customers and employees to make an impact through their work"},
+  {image: Excellence,title: "Excellence and dependable", description: "We surpass our customers' expecta tions in a caring and professional manner. "},
+  {image: PositivePassionate,title: "Positive & passionate", description: "We are a cheerful people, passionate about our brand and mission. We believe in possibilities!"},
+  {image: SwiftResponsive,title: "Swift and Responsive", description: "We are swift and responsive by design. We prioritize a personalized customer experience."},
+  {image: Innovative,title: "Innovative", description: "We encourage collaborations and constantly seek new innovative solutions to our challenges."},
+]
+
 const AboutDetail: React.FC<AboutDetailProp> = ({ currentSelected }) => {
   return (
     <StyledAboutDetail>
       {currentSelected === "overview" ? (
-        <AboutCard heading={"Company Overview"} descriptionList={descriptionData}/>
+        <AboutCard heading={"Company Overview"} descriptionList={overviewDescription}/>
       ) : currentSelected === "values" ? (
-        <h1>values</h1>
+        <>
+        <AboutCard heading={"Mission & Vision"} descriptionInfo={missionData} />
+        <AboutCard heading={"Values"} descriptionInfo={valuesData} />
+        </>
       ) : currentSelected === "business" ? (
-        <h1>business</h1>
+        <AboutCard heading={"Our businesses"} descriptionInfo={businessData} />
       ) : null}
     </StyledAboutDetail>
   );
