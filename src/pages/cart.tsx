ce54fa9d886d8b1ab57823b86b53dff804430706
Solozo <ozoemenamsolomon@yahoo.com>
2022-12-22 10:03:33 +0100

    wip: implement cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,15 +1,24 @@
 import FilterPanel, { FilterType } from '@components/FilterPanel';
-import ProductCard from '@components/ProductCard';
-import ProductsPanel from '@components/ProductsPanel';
+import ProductCard, { priceFormatter } from '@components/ProductCard';
 import SooSection from '@components/SooSection';
 import { Container } from '@components/styled';
-import FilterProductContainer from '@components/styled/FIlterProductContainer';
 import { ProductDataType, productsData } from '@data/productsData';
 import { GetStaticProps, NextPage } from 'next';
-import React, { useState } from 'react';
 import Image from 'next/image';
-import { ImageContainer } from '@components/ServiceCard';
-import CTA from '@components/CTA';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Trash } from 'styled-icons/heroicons-outline';
+import {
+	ProductCountControlButton,
+	ProductCountInput,
+} from './composites/products/polyester-resin';
+
+const formatGramm = new Intl.NumberFormat('en-US', {
+	style: 'unit',
+	unit: 'gram',
+	notation: 'compact',
+	unitDisplay: 'narrow',
+});
 
 const Cart: NextPage<{
 	products: ProductDataType[];
@@ -22,34 +31,95 @@ const Cart: NextPage<{
 		},
 	});
 
+	const isInStock = false;
+
+	const [productCount, setProductCount] = useState(0);
+
 	const removeFromCart = (id: string) => () => {
 		console.log({ idToRemove: id });
 	};
 
+	console.log({ weight: products[0].weightInGrams });
+
 	return (
 		<Container>
-			<div>Cart</div>
 			<div>
-				<h2>Your cart (3 items)</h2>
 				<div style={{ display: 'flex' }}>
-					<div>
-						{Array.from({ length: 3 }, () => productsData[3]).map(
-							(product, index) => (
+					<div
+						style={{
+							flex: 1,
+						}}>
+						<Title>Your cart (3 items)</Title>
+						{products.map(({ previewImages, ...product }, index) => (
+							<CartItemWrapper>
 								<div>
-									<ImageContainer>
-										<Image src={product.image} alt="product image" />
-									</ImageContainer>
-									<p>{product.name}</p>
-									<p>{product.price}</p>
-									<CTA onClick={removeFromCart(product.code)}>Remove</CTA>
-
-									{/* <p
-										key={`product_${index}`}
-										children={JSON.stringify(product, null, 2)}
-									/> */}
+									<div>
+										<ImageContainer>
+											<Image src={product.image} alt="product image" />
+										</ImageContainer>
+										<button
+											style={{
+												display: 'flex',
+												alignItems: 'center',
+												gap: '0.21rem',
+												background: 'none',
+												border: 'none',
+												color: 'var(--oex-danger)',
+											}}
+											onClick={removeFromCart(product.code)}>
+											<Trash size={18} />
+											Remove
+										</button>
+									</div>
+									<div>
+										<h3 style={{}}>{product.name}</h3>
+										<p style={{ fontSize: '1.2rem', color: 'var(--oex-grey)' }}>
+											Size: {formatGramm.format(product.weightInGrams)}
+										</p>
+										{!isInStock ? (
+											<p style={{ color: 'var(--oex-danger)' }}>Out of stock</p>
+										) : (
+											<p style={{ color: 'var(--oex-success)' }}>In stock</p>
+										)}
+									</div>
 								</div>
-							),
-						)}
+
+								<div>
+									<div
+										style={{
+											display: 'flex',
+											gap: '.5rem',
+											alignItems: 'center',
+										}}>
+										<ProductCountControlButton
+											onClick={() => {
+												console.log(productCount);
+												setProductCount(
+													prevProductCount => prevProductCount - 1,
+												);
+											}}>
+											-
+										</ProductCountControlButton>
+										<ProductCountInput
+											type="number"
+											name="quantity"
+											id="quantity"
+											value={productCount}
+											onChange={e => setProductCount(Number(e.target.value))}
+										/>
+										<ProductCountControlButton
+											onClick={() =>
+												setProductCount(
+													prevProductCount => prevProductCount + 1,
+												)
+											}>
+											+
+										</ProductCountControlButton>
+									</div>
+									<Price>{priceFormatter.format(product.price)}</Price>
+								</div>
+							</CartItemWrapper>
+						))}
 					</div>
 					<FilterPanel {...{ filter, setFilter, noCategory: true }} />
 				</div>
@@ -92,3 +162,38 @@ export const getStaticProps: GetStaticProps = async context => {
 		},
 	};
 };
+
+const Title = styled.h2`
+	margin: 0;
+	font-weight: 600;
+	font-size: 1.2rem;
+	border-bottom: 1px solid var(--oex-light-grey);
+	padding-bottom: 0.5rem;
+	margin-bottom: 1rem;
+	display: flex;
+`;
+
+const CartItemWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem;
+	border-bottom: 1px solid var(--oex-light-grey);
+
+	& > div:first-child {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+`;
+
+const Price = styled.p`
+	font-weight: 600;
+	font-size: 1.5rem;
+	text-align: right;
+	margin-block: 1rem;
+`;
+
+const ImageContainer = styled.div`
+	position: relative;
+`;
