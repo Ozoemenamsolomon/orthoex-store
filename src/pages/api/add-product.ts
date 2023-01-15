af4f9212c244286984586a23061328130439885d
solozo <ozoemenamsolomon@yahoo.com>
2023-01-15 16:49:00 +0100

    disable add product page

--- src/pages/api/add-product.ts
+++ src/pages/api/add-product.ts
@@ -1,46 +1,67 @@
-import { supabaseClient } from '@utils/supabase';
+// import { supabaseClient } from '@utils/supabase';
 import { NextApiHandler } from 'next';
 
 const handler: NextApiHandler = async (req, res) => {
-	const product = req.body;
+	// const product = req.body;
 
-	if (req.method !== 'POST') {
+	if (true) {
 		return res.status(405).json({ error: 'Method not allowed' });
 	}
 
-	if (req.cookies.temp_admin_cookie !== process.env.ADMIN_COOKIE_VALUE) {
-		return res.status(401).json({ error: 'Unauthorized' });
-	}
+	// if (req.cookies.temp_admin_cookie !== process.env.ADMIN_COOKIE_VALUE) {
+	// 	return res.status(401).json({ error: 'Unauthorized' });
+	// }
 
-	if (
-		!product.name ||
-		!product.code ||
-		!product.price ||
-		!product.brand ||
-		!product.category
-	) {
-		return res.status(400).json({ error: 'Missing required fields' });
-	}
+	// if (!product.name || !product.code || !product.brand || !product.category) {
+	// 	console.log({ product });
+	// 	return res.status(400).json({ error: 'Missing required fields' });
+	// }
 
-	const { data, error } = await supabaseClient.from('products').insert([
-		{
-			name: product.name,
-			code: product.code,
-			image: product.image,
-			price: product.price,
-			description: product.description,
-			details: product.details,
-			brand: product.brand,
-			category: product.category,
-		},
-	]);
+	// const { data, error: productInsertError } = await supabaseClient
+	// 	.from('products')
+	// 	.insert([
+	// 		{
+	// 			name: product.name,
+	// 			code: product.code,
+	// 			image: product.image,
+	// 			description: product.description,
+	// 			details: product.details,
+	// 			brand: product.brand,
+	// 			category: product.category,
+	// 		},
+	// 	])
+	// 	.select('*')
+	// 	.single();
 
-	if (error) {
-		console.log('error: ', error);
-		return res.status(500).json({ error: error.details || error.message });
-	}
+	// if (productInsertError || !data) {
+	// 	console.log('error: ', productInsertError);
+	// 	return res.status(500).json({
+	// 		error:
+	// 			productInsertError?.details ||
+	// 			productInsertError?.message ||
+	// 			"couldn't add product",
+	// 	});
+	// }
+	// const addedProductCode = data?.code;
+	// return res.status(200).json({ productCode: addedProductCode });
 
-	return res.status(200).json({ data });
+	// const pricesToCustierMap = Object.entries(product.prices).map(
+	// 	([key, value]) => ({
+	// 		product: addedProductCode,
+	// 		custier: key,
+	// 		price: value,
+	// 	}),
+	// );
+
+	// const { data: _pricesInsertData, error: pricesInsertError } =
+	// 	await supabaseClient.from('prices').insert(pricesToCustierMap);
+
+	// if (pricesInsertError) {
+	// 	console.log('error: ', pricesInsertError);
+	// 	return res.status(500).json({
+	// 		error: pricesInsertError.details || pricesInsertError.message,
+	// 	});
+	// }
 };
 
 export default handler;
