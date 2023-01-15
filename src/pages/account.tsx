c43e0155423d9239b977d595033f03985d3af762
solozo <ozoemenamsolomon@yahoo.com>
2023-01-15 16:49:00 +0100

    disable add product page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -19,6 +19,46 @@ const Account: NextPage<Props> = ({ user, data }) => {
 			<p>You are logeged in with {user.email} </p>
 			<p>name: {user.name}</p>
 			<img src={user.picture || ''} alt="user gravatar" />
+			<hr />
+			{data.map(
+				({
+					name,
+					code,
+					variants,
+					description,
+					brand,
+					category,
+					details,
+				}: any) => (
+					<div key={code}>
+						<h2>{name}</h2>
+						<p>Brand: {brand.name}</p>
+						<p>Category: {category.name}</p>
+						<p>Description: {description}</p>
+						<p>Details: {details}</p>
+						<p>Code: {code}</p>
+						<hr />
+						<div>
+							<h5>Variations</h5>
+							<ol>
+								{variants?.map(({ variant, quantity, prices, id }: any) => (
+									<li key={'variant' + id}>
+										<p>Variant id: {id}</p>
+										{Object.entries(variant).map(([key, value]) => (
+											<p key={key}>
+												{key}: {value}
+											</p>
+										))}
+										<p>Qty. in stock: {quantity?.quantity}</p>
+										<p>Price: {prices[0]?.priceInKobo}</p>
+									</li>
+								))}
+							</ol>
+						</div>
+						<hr />
+					</div>
+				),
+			)}
 		</Container>
 	);
 };
@@ -31,15 +71,15 @@ export const getServerSideProps = withPageAuthRequired({
 
 		const custier = session?.user.custier;
 
-		console.log({ custier });
+		if (process.env.NODE_ENV !== 'development') return { props: { data: [] } };
 
 		// @ts-ignore
 		const { data, error } = await supabaseClient
 			.from('products')
 			.select(
-				`code, name, image, description, brand(name, slug), category(name, slug, image), details,variants(weightInGram, hardness, volumeInML, colour, gms, material, prices(custier, priceInKobo),quantity(quantity))`,
-			);
-		// .eq('prices.variant.custier', custier);
+				`code, name, image, description, brand(name, slug), category(name, slug, image), details,variants(variant, id, prices(custier, priceInKobo),quantity(quantity))`,
+			)
+			.eq('variants.prices.custier', custier);
 
 		if (error) {
 			console.log({ error });
