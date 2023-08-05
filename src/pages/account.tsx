40c89d10f100c756afb2b04564f1f3750835b727
solozo <ozoemenamsolomon@yahoo.com>
2023-08-05 22:36:57 +0200

    remove db preview data from account

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,29 +2,14 @@ import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { UserProfile } from '@auth0/nextjs-auth0/client';
 import { CTALink } from '@components/CTA';
 import { Container } from '@components/styled';
-import { supabaseClient } from '@utils/supabase';
 import { NextPage } from 'next';
 
 type Props = {
 	user: UserProfile;
-	data: {
-		name: string;
-		brand: { name: string };
-		category: { name: string };
-		description: string;
-		details: string;
-		code: string;
-		variant: { [key: string]: string };
-		quantity: number;
-		price: { priceInKobo: number };
-		image: string;
-		variantID: string;
-	}[];
-	trainings: any[];
 };
 
-const Account: NextPage<Props> = ({ user, data, trainings }) => {
-	console.log({ user, trainings });
+const Account: NextPage<Props> = ({ user }) => {
+	console.log({ user });
 
 	return (
 		<Container>
@@ -36,52 +21,6 @@ const Account: NextPage<Props> = ({ user, data, trainings }) => {
 			<hr />
 			<textarea name="" id=""></textarea>
 			<hr />
-			{data.map(
-				({
-					name,
-					brand,
-					category,
-					description,
-					details,
-					code,
-					variant,
-					quantity,
-					price,
-					image,
-					variantID,
-				}) => (
-					<div key={`product_number_${variantID + code}`}>
-						<p
-							style={{
-								padding: '6px',
-								borderRadius: '3px',
-								border: '1px solid var(--oex-orange-dark)',
-								backgroundColor: 'var(--oex-danger)',
-								color: 'white',
-							}}>
-							Your are seeing this because you&apos;ve been added to the test
-							user
-						</p>
-						<h2>{name}</h2>
-						<p>Brand: {brand.name}</p>
-						<p>Category: {category.name}</p>
-						<p>Code: {code}</p>
-						<p>Price: {price.priceInKobo}</p>
-						<p>Description: {description}</p>
-						<p>Details: {details}</p>
-						<p>Qty. in stock: {quantity}</p>
-						<p>VariantID:{variantID}</p>
-						<p>Image: {image}</p>
-						<hr />
-						{Object.entries(variant).map(([key, value]) => (
-							<p key={key}>
-								{key}: {value}
-							</p>
-						))}
-						<hr />
-					</div>
-				),
-			)}
 		</Container>
 	);
 };
@@ -94,45 +33,15 @@ export const getServerSideProps = withPageAuthRequired({
 
 		const custier = session?.user.custier;
 
+		console.log({ custier });
 		if (
 			!process.env.ALLOWED_USER_EMAIL?.split(',').includes(session?.user.email)
 		) {
-			return { props: { data: [], trainings: [] } };
-		}
-
-		// @ts-ignore
-		const { data, error } = await supabaseClient
-			.from('variants')
-			.select(
-				`*, variantID:id, product(code, name, image, description, details, brand(name, slug), category(name, slug, image)), quantity(quantity),price:prices(*)`,
-			)
-			.eq('prices.custier', custier);
-
-		const { data: trainings, error: error2 } = await supabaseClient
-			.from('training')
-			.select('*');
-
-		if (error || error2) {
-			console.log({ error });
-			return {
-				props: {
-					data: [],
-					trainings: [],
-				},
-			};
+			return { props: {} };
 		}
 
 		return {
-			props: {
-				data: data?.map(({ price, product, quantity, variantID, variant }) => ({
-					price: price[0],
-					variantID,
-					variant,
-					...quantity,
-					...product,
-				})),
-				trainings,
-			},
+			props: {},
 		};
 	},
 });
