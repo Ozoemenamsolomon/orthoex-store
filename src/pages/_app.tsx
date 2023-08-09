1045c7700d4bc075f0eff6c7fdf8bef8e192e89e
solozo <ozoemenamsolomon@yahoo.com>
2023-08-09 22:32:59 +0200

    Add google tag script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { theme } from '@styles/theme';
 import { CartProvider } from 'context/cartContext';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from 'styled-components';
@@ -9,17 +10,37 @@ import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 	return (
-		<CartProvider>
-			<UserProvider>
+		<UserProvider>
+			<CartProvider>
 				<ThemeProvider theme={theme}>
 					<Layout>
+						{process.env.NODE_ENV === 'production' && GA_MEASUREMENT_ID && (
+							<>
+								<Script
+									src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+									strategy="afterInteractive"
+								/>
+								<Script id="google-analytics">
+									{`
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', 'GA_MEASUREMENT_ID');`}
+								</Script>
+								{/* include paystack */}
+								<Script
+									src="https://js.paystack.co/v1/inline.js"
+									strategy="afterInteractive"></Script>
+							</>
+						)}
 						<Component {...pageProps} />
 						<ToastContainer />
 					</Layout>
 				</ThemeProvider>
-			</UserProvider>
-		</CartProvider>
+			</CartProvider>
+		</UserProvider>
 	);
 }
 export default MyApp;
