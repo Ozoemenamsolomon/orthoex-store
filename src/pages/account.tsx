ced5ab7b9e882e1ffa226c122105d0d0eba533aa
solozo <ozoemenamsolomon@yahoo.com>
2023-08-11 19:10:38 +0200

    display orders in account page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -6,7 +6,6 @@ import { Container } from '@components/styled';
 import { supabaseClient } from '@utils/supabase';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { Key } from 'react';
 
 type Props = {
 	user: UserProfile;
@@ -19,13 +18,12 @@ const Account: NextPage<Props> = ({ user, orders }) => {
 		<Container>
 			<h1>Account</h1>
 			<CTALink href="/api/auth/logout">Logout</CTALink>
-			<p>You are logeged in with {user.email} </p>
+			<p>You are logeged in as {user.email} </p>
 			<p>name: {user.name}</p>
 			<img src={user.picture || ''} alt="user gravatar" />
 			<hr />
 			<textarea name="" id=""></textarea>
 			<hr />
-			{/* <pre>{JSON.stringify(orders, null, 2)}</pre> */}
 			<h2>
 				{orders.length} order{orders.length > 1 ? 's' : ''}
 			</h2>
@@ -37,23 +35,74 @@ const Account: NextPage<Props> = ({ user, orders }) => {
 						margin: '1rem 0',
 						padding: '1rem',
 						display: 'flex',
-						justifyContent: 'space-between',
+						flexDirection: 'column',
 					}}>
-					{order.cart.map((item: { id: Key | null | undefined }) => (
-						<span>
-							<pre key={item.id}>{JSON.stringify(item, null, 1)}</pre>
-						</span>
-					))}
-					<div style={{ display: 'flex', alignItems: 'center' }}>
-						{!order.paid && (
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'space-between',
+							backgroundColor: 'var(--oex-orange-mute)',
+							marginTop: '-1rem',
+							marginInline: '-1rem',
+							padding: '1rem',
+							paddingBottom: 0,
+						}}>
+						<p>Order placed on {order.created_at}</p>
+						<p>
+							Totalling {priceFormatter.format(order.totalPrice)} for{' '}
+							{order.cart.length} item{order.cart.length > 1 ? 's' : ''}
+						</p>
+						<p>
+							Order reference: <strong>{order.reference}</strong>
+						</p>
+					</div>
+					<div>
+						{order.cart.map(
+							(item: {
+								id: number;
+								quantity: number;
+								name: string;
+								price: number;
+								variant: any;
+							}) => (
+								<div
+									style={{
+										display: 'grid',
+										gridTemplateColumns: 'repeat(4,1fr)',
+										gap: '1rem',
+										padding: '1rem',
+										borderBottom: '1px solid #ccc',
+									}}
+									key={item.id}>
+									<span>
+										{item.quantity} x {item.name}
+									</span>
+
+									<span>{priceFormatter.format(item.price)}</span>
+									<span>
+										{priceFormatter.format(item.price * item.quantity)}
+									</span>
+
+									{JSON.stringify(item.variant, null, 1)}
+								</div>
+							),
+						)}
+					</div>
+					{!order.paid && (
+						<div style={{ display: 'flex', alignItems: 'center' }}>
 							<CTA
 								onClick={() => {
 									router.push(`/composites/checkout/${order.reference}`);
 								}}>
 								Pay {priceFormatter.format(order.totalPrice)}
 							</CTA>
-						)}
-					</div>
+							<p>
+								Order expires by{' '}
+								{new Date(order.expiresAt).toLocaleTimeString()} on{' '}
+								{new Date(order.expiresAt).toLocaleDateString()}
+							</p>
+						</div>
+					)}
 				</div>
 			))}
 		</Container>
