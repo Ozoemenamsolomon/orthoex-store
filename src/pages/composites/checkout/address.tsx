ae0bd4a208e551a4f4b2a46ca03a83c2f9ad1f72
solozo <ozoemenamsolomon@yahoo.com>
2023-08-26 22:44:37 +0200

    WIP: Delivery fee estimation - style address form

--- src/pages/composites/checkout/address.tsx
+++ src/pages/composites/checkout/address.tsx
@@ -4,10 +4,11 @@ import { AccountDetailsForm } from '@components/account/Details';
 import { Container } from '@components/styled';
 import { FormControl } from '@components/styled/Forms';
 import { Title } from '@components/styled/Temp';
-import { singleDBProductToProductMapper } from '@data/productsData';
+import { formatPrice } from '@utils/index';
 import { useCart } from 'context/cartContext';
 import { NextPage } from 'next';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 const Address: NextPage<{
 	states: {
@@ -22,16 +23,15 @@ const Address: NextPage<{
 				state?: string;
 				lga?: string;
 				phone?: string;
+				deliveryOption?: string;
 		  }
 		| undefined
 	>(undefined);
 
 	const formSubmitable =
-		address?.CO &&
-		address?.streetAdress &&
-		address?.state &&
-		address?.lga &&
-		address?.phone;
+		address?.CO && address?.phone && address?.deliveryOption === 'waybill'
+			? address?.streetAdress && address?.state && address?.lga
+			: true;
 
 	const { cart, cartProducts, checkout } = useCart({
 		withProductDetails: true,
@@ -44,12 +44,10 @@ const Address: NextPage<{
 		}[]
 	>([]);
 
-	const transformedProducts = cartProducts.map(product => ({
-		...singleDBProductToProductMapper(product),
-		quantity:
-			cart.find(item => item.productVariantID === product.variantID.toString())
-				?.quantity || 0,
-	}));
+	const [deliveryFee, setDeliveryFee] = useState(0);
+	const subTotal =
+		cartProducts.reduce((acc, item) => acc + item.price * item.quantity, 0) ||
+		0;
 
 	return (
 		<Container
@@ -57,8 +55,13 @@ const Address: NextPage<{
 			style={{
 				display: 'flex',
 				gap: '2rem',
+				flexWrap: 'wrap',
+				alignContent: 'stretch',
 			}}>
-			<div>
+			<AddressFormWrapper
+				style={{
+					flex: 3,
+				}}>
 				<Title>Delivery adddress</Title>
 				<AccountDetailsForm
 					onSubmit={e => {
@@ -72,88 +75,170 @@ const Address: NextPage<{
 						checkout(address);
 					}}>
 					<FormControl>
-						<label htmlFor="state">Full name</label>
+						<label htmlFor="fullName">Full name</label>
 						<input
 							type="text"
 							placeholder="John Doe"
 							value={address?.CO}
+							id="fullName"
 							onChange={e => setAddress({ ...address, CO: e.target.value })}
 						/>
 					</FormControl>
 					<FormControl>
-						<label htmlFor="state">Phone</label>
+						<label htmlFor="phone">Phone</label>
 						<input
 							type="text"
 							placeholder="+2347000000000"
+							id="phone"
 							value={address?.phone}
 							onChange={e => setAddress({ ...address, phone: e.target.value })}
 						/>
 					</FormControl>
-					<FormControl
+					<div
 						style={{
-							width: '100%',
+							display: 'flex',
+							gap: '2rem',
 						}}>
-						<label htmlFor="address">Address</label>
-						<textarea
-							id="address"
-							placeholder="Street, number, city"
-							name="streetAdress"
-							value={address?.streetAdress}
-							onChange={e =>
-								setAddress({ ...address, streetAdress: e.target.value })
-							}
-						/>
-					</FormControl>
-					<FormControl>
-						<label htmlFor="state">State</label>
-						<select
-							name="state"
-							id="state"
-							onChange={e => {
-								setAddress({ ...address, state: e.target.value });
-								fetch(`/api/lgas?state=${e.target.value}`)
-									.then(res => res.json())
-									.then(data => {
-										setLga(data);
-									});
-							}}>
-							<option value="">Select State</option>
-							{states.map((state, index) => (
-								<option key={index} value={state.id}>
-									{state.name}
-								</option>
-							))}
-						</select>
-					</FormControl>
-					<FormControl>
-						<label htmlFor="lga">LGA</label>
-						<select
-							name="lga"
-							id="lga"
-							onChange={e => setAddress({ ...address, lga: e.target.value })}>
-							<option value="">Select LGA</option>
-							{lga.map((lga, index) => (
-								<option key={index} value={lga.id}>
-									{lga.name}
-								</option>
-							))}
-						</select>
-					</FormControl>
+						<FormControl>
+							<label htmlFor="selfPickup">Pickup</label>
+							<input
+								type="radio"
+								name="deliveryOption"
+								id="selfPickup"
+								value="selfPickup"
+								onChange={e =>
+									setAddress({ ...address, deliveryOption: e.target.value })
+								}
+							/>
+						</FormControl>
+						<FormControl>
+							<label htmlFor="waybill">Waybill</label>
+							<input
+								type="radio"
+								name="deliveryOption"
+								id="waybill"
+								value="waybill"
+								onChange={e =>
+									setAddress({ ...address, deliveryOption: e.target.value })
+								}
+							/>
+						</FormControl>
+					</div>
+					{address?.deliveryOption === 'waybill' && (
+						<>
+							<FormControl
+								style={{
+									width: '100%',
+								}}>
+								<label htmlFor="address">Address</label>
+								<textarea
+									id="address"
+									placeholder="House no., Street name, City"
+									name="streetAdress"
+									value={address?.streetAdress}
+									onChange={e =>
+										setAddress({ ...address, streetAdress: e.target.value })
+									}
+								/>
+							</FormControl>
+							<FormControl>
+								<label htmlFor="state">State</label>
+								<select
+									name="state"
+									id="state"
+									onChange={e => {
+										setAddress({ ...address, state: e.target.value });
+										fetch(`/api/lgas?state=${e.target.value}`)
+											.then(res => res.json())
+											.then(data => {
+												setLga(data);
+											});
+									}}>
+									<option value="">Select State</option>
+									{states.map((state, index) => (
+										<option key={index} value={state.id}>
+											{state.name}
+										</option>
+									))}
+								</select>
+							</FormControl>
+							<FormControl>
+								<label htmlFor="lga">LGA</label>
+								<select
+									name="lga"
+									id="lga"
+									onChange={e => {
+										setAddress({ ...address, lga: e.target.value });
+										//if value is not empty
+
+										{
+											//  estimate delivery fee
+											const estimatedDeliveryFee = 0;
+
+											setDeliveryFee(estimatedDeliveryFee);
+										}
+									}}>
+									<option value="">Select LGA</option>
+									{lga.map((lga, index) => (
+										<option key={index} value={lga.id}>
+											{lga.name}
+										</option>
+									))}
+								</select>
+							</FormControl>
+						</>
+					)}
 
 					<CTA>Place order</CTA>
 				</AccountDetailsForm>
-			</div>
-			<div>
-				<Title>Your order ({cart.length} items)</Title>
-				{transformedProducts.map((item: any, index: any) => (
-					<CartItem readOnly key={`checkout-item-${index}`} {...item} />
-				))}
-			</div>
+			</AddressFormWrapper>
+			<OrderDetailsWrapper>
+				<div
+					style={{
+						flex: 1,
+					}}>
+					<Title>Your order ({cart.length} items)</Title>
+					{cartProducts.map((item: any, index: any) => (
+						<CartItem readOnly key={`checkout-item-${index}`} {...item} />
+					))}
+				</div>
+				<p>
+					Subtotal:
+					<span>{formatPrice(subTotal)}</span>
+				</p>
+				<p>
+					Delivery fee:
+					<span>{!deliveryFee ? '----' : formatPrice(deliveryFee)}</span>
+				</p>
+				<p>
+					Total: <span>{formatPrice(subTotal + deliveryFee)}</span>
+				</p>
+			</OrderDetailsWrapper>
 		</Container>
 	);
 };
 
 export default Address;
+
+const OrderDetailsWrapper = styled.div`
+	flex: 1;
+	box-shadow: 2px 2px 9px 1px rgb(0 0 0 / 10%);
+	border-radius: 5px;
+	padding: 1rem;
+	display: flex;
+	flex-direction: column;
+
+	> p {
+		display: flex;
+		justify-content: space-between;
+	}
+`;
+const AddressFormWrapper = styled.div`
+	flex: 3;
+	box-shadow: 2px 2px 9px 1px rgb(0 0 0 / 10%);
+	border-radius: 5px;
+	padding: 1rem;
+`;
 
 export const getServerSideProps = async () => {
 	const states = [
