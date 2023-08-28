18763c67fcf9e57df76cf60b66e78e78c63509a5
solozo <ozoemenamsolomon@yahoo.com>
2023-08-28 16:12:40 +0200

    fix supabase issue

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseKey = process.env.SUPABASE_KEY || '';
-const supabaseTrainingUrl = process.env.SUPABASE_TRAINING_URL || '';
-const supabaseTrainingKey = process.env.SUPABASE_TRAINING_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+const supabaseTrainingUrl = process.env.NEXT_PUBLIC_SUPABASE_TRAINING_URL || '';
+const supabaseTrainingKey = process.env.NEXT_PUBLIC_SUPABASE_TRAINING_KEY || '';
 
 export const supabaseClient = createClient(supabaseUrl, supabaseKey);
 export const supabaseTrainingClient = createClient(
