466ed6542c58496cacd4399b5581dc62ff94d0ab
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2023-09-23 12:28:05 +0200

    update order sucessful page

--- src/pages/trainings/checkout/[reference].tsx
+++ src/pages/trainings/checkout/[reference].tsx
@@ -31,7 +31,7 @@ const CheckoutPage: NextPage<{
 
 	const initializePayment = usePaystackPayment(config);
 	const [isSuccessful, setIsSuccessful] = useState(false);
-	const [paidOrder, setPaidOrder] = useState<TrainingOrderType | null>(null);
+	const [_, setPaidOrder] = useState<TrainingOrderType | null>(null);
 
 	const isxpired = new Date(trainingOrder.expiredAt).getTime() < Date.now();
 
@@ -91,33 +91,34 @@ const CheckoutPage: NextPage<{
 								<h5 className="title">Thanks for your order</h5>
 								<span className="order-details">
 									Order ID:
-									<span className="order-id">#{paidOrder?.id}</span>
+									<span className="order-id">#{trainingOrder?.id}</span>
 								</span>
 							</ThankYouBox>
 						</Header>
 						<OrderInfoDetails>
 							<InfoTitle>
 								<p className="title">You are attending</p>
-								<p className="description">{paidOrder?.title}</p>
+								<p className="description">{trainingOrder?.title}</p>
 							</InfoTitle>
 							<MetaDetails>
 								<MetaData>
 									<span className="title">payment info:</span>
-									<span className="description">{paidOrder?.user}</span>
+									<span className="description">{trainingOrder?.user}</span>
 								</MetaData>
 								<MetaData>
 									<span className="title">Date:</span>
 									<span className="description">{`${formatDate(
-										new Date(paidOrder?.trainingDate as string),
+										new Date(trainingOrder?.trainingDate as string),
 									)} , ${formatTime(
-										new Date(paidOrder?.trainingDate as string),
+										new Date(trainingOrder?.trainingDate as string),
 									)}`}</span>
 								</MetaData>
 								<MetaData>
 									<span className="title">Location:</span>
-									<span className="description">{paidOrder?.location}</span>
+									<span className="description">{trainingOrder?.location}</span>
 								</MetaData>
 							</MetaDetails>
+							<Text>The training ticket and address will be sent to participants email.</Text>
 						</OrderInfoDetails>
 					</Container>
 				</OrderSuccess>
@@ -132,15 +133,15 @@ const CheckoutPage: NextPage<{
 					<OrderInfo>
 						<h5 className="title">Orders</h5>
 						<OrderDetails>
-							<span className="description">1x Subtotal</span>
+							<span className="description">{`${trainingOrder.numOfParticipants}`}x Subtotal</span>
 							<span className="amount">
 								{priceFormatter.format(trainingOrder.trainingPrice)}
 							</span>
 						</OrderDetails>
 						<OrderDetails>
-							<span className="description">1x Discount</span>
+							<span className="description">{`${trainingOrder.numOfParticipants}`}x Discount</span>
 							<span className="amount">
-								- {priceFormatter.format(trainingOrder.discount)}
+								- {priceFormatter.format(trainingOrder.discount * trainingOrder.numOfParticipants)}
 							</span>
 						</OrderDetails>
 						<OrderDetails>
@@ -372,4 +373,8 @@ const MetaData = styled.div`
 
 	@media ${({ theme }) => theme.breakpoints.above.md} {
 	}
+`;
+
+const Text = styled.p`
+	margin: 2rem 0;
 `;
